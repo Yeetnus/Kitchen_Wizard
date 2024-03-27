3615557d78bfcb6fead7e0be11fbdc2ef31c63ba
uNastyy_ <bouissouromain82@gmail.com>
2024-03-27 11:29:52 +0100

    favoris

--- app/src/main/java/com/example/kitchenwizard/Favoris.java
+++ app/src/main/java/com/example/kitchenwizard/Favoris.java
@@ -1,65 +1,50 @@
 package com.example.kitchenwizard;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+public class Favoris extends Activity {
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-public class Favoris {
-
-    public void doSomething(String valeur) {
-        Intent intent = new Intent(this, Recette.class);
-        intent.putExtra("pokemon", valeur);
-        startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.allergenes) {
+            Intent intent = new Intent(this, Allergenes.class);
+            startActivity(intent);
+            return true;
+        } else if (item.getItemId() == R.id.recettes) {
+            Intent intent = new Intent(this, MainActivity.class);
+            return true;
+        } else if (item.getItemId() == R.id.favoris) {
+            Intent intent = new Intent(this, Favoris.class);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.favoris);
 
-
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        List<String> listItem = new ArrayList<>();
-        ListView listView = (ListView) findViewById(R.id.list);
-        ArrayAdapter<String> adapter =  new ArrayAdapter<>(this, R.layout.list_recette, R.id.textView, listItem);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Récupérer l'élément sélectionné à partir de l'adaptateur de la liste
-                String valeurSelectionnee = (String) parent.getItemAtPosition(position);
-                doSomething(valeurSelectionnee);
-            }
-        });
-
-        Button bouton=findViewById(R.id.buttonoui);
-        bouton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String uniqueId = UUID.randomUUID().toString();
-                listItem.add(uniqueId);
-                adapter.notifyDataSetChanged();
-            }
-        });
     }
+
+    public void doSomething(View view) {
+        finish();
+    }
+
 }
