eb804ad026d98e8c27906a28e31a6bb022b6fb79
uNastyy_ <bouissouromain82@gmail.com>
2024-03-27 12:12:13 +0100

    allergenes

--- app/src/main/java/com/example/kitchenwizard/Allergenes.java
+++ app/src/main/java/com/example/kitchenwizard/Allergenes.java
@@ -1,4 +1,50 @@
 package com.example.kitchenwizard;
 
-public class Allergenes {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class Allergenes extends Activity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.allergenes) {
+            Intent intent = new Intent(this, Allergenes.class);
+            startActivity(intent);
+            return true;
+        } else if (item.getItemId() == R.id.recettes) {
+            Intent intent = new Intent(this, MainActivity.class);
+            return true;
+        } else if (item.getItemId() == R.id.favoris) {
+            Intent intent = new Intent(this, Favoris.class);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favoris);
+
+    }
+
+    public void doSomething(View view) {
+        finish();
+    }
+
 }
