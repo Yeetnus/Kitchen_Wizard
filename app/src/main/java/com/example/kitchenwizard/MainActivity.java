2cbe69bcbf21dc3e20feea2988f033c1c926d30b
Yeetnus <yanis.croguennoc@etu.iut-tlse3.Fr>
2024-03-27 11:36:14 +0100

    IHM main

--- app/src/main/java/com/example/kitchenwizard/MainActivity.java
+++ app/src/main/java/com/example/kitchenwizard/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kitchenwizard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,6 +22,30 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MonTagDeLActivité";
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.allergenes) {
+            Intent intent = new Intent(this, Allergenes.class);
+            startActivity(intent);
+            return true;
+        } else if (item.getItemId() == R.id.recettes) {
+            Intent intent = new Intent(this, MainActivity.class);
+            return true;
+        } else if (item.getItemId() == R.id.favoris) {
+            Intent intent = new Intent(this, Favoris.class);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
     public void doSomething(String valeur) {
         Intent intent = new Intent(this, Recette.class);
         intent.putExtra("pokemon", valeur);
